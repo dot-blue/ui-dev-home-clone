53d1b47c94e338b6a5ea10bb78004450ce712206
iwritecodesometimes <121631567+dot-blue@users.noreply.github.com>
2023-01-10 22:09:31 +0200

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,21 +3,32 @@ import React from "react";
 function Hero() {
   return (
     <div className="hero-container">
-      <div className="hero-left">
-        <h1>Welcome, Friend</h1>
-        <h2>Stuck in tutorial hell?</h2>
-        <p>
-          You’ve come to the right place. We make learning the JavaScript
-          ecosystem fun, approachable, and easy...
-        </p>
-        <a href="#">
-          <span>Shut up and take my money</span>
-        </a>
+      <div className="hero-left-container">
+        <div className="hero-left">
+          <header>
+            <h1>Welcome, Friend</h1>
+            <h2>Stuck in tutorial hell?</h2>
+          </header>
+
+          <p>
+            You’ve come to the right place. We make learning the JavaScript
+            ecosystem fun, approachable, and easy...
+          </p>
+
+          <div className="hero-cta">
+            <a href="#">
+              <span>Shut up and take my money</span>
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="hero-right">
-        <div className="card card1">Card 1</div>
-        <div className="card card2">Card 2</div>
-        <div className="card card3">Card 3</div>
+
+      <div className="hero-right-container">
+        <div className="hero-right">
+          <div className="card card1">Card 1</div>
+          <div className="card card2">Card 2</div>
+          <div className="card card3">Card 3</div>
+        </div>
       </div>
     </div>
   );
