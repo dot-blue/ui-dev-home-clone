c824c2c139fdc960ab2ef7d2e5f2c724d76b7f0c
iwritecodesometimes <121631567+dot-blue@users.noreply.github.com>
2023-01-19 22:25:42 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
+import Logos from "./components/Logos";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Hero />
+      <Logos />
     </div>
   );
 }
---
+++ src/components/Logos.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logoList from "../assets/logo-list.svg";
+
+function Logos() {
+  return (
+    <div className="logos-container">
+      <figure className="logos-figure">
+        <img src={logoList} alt="" className="company-logos" />
+        <figcaption>
+          You’ll be in great company with other developers we’ve trained
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
+
+export default Logos;
