11afc4635d8860032c45f259f140efe76a99c11c
iwritecodesometimes <121631567+dot-blue@users.noreply.github.com>
2023-01-21 19:40:38 +0200

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 
 function Hero() {
+  const [width, setWidth] = useState(0);
+  const sliderWrapper = useRef();
+
+  const cards = [
+    { id: 1, className: "card  card-active", name: "card1", draggable: "x" },
+    { id: 2, className: "card  card-left", name: "card2", draggable: "" },
+    { id: 3, className: "card  card-right", name: "card3", draggable: "" },
+  ];
+
+  useEffect(() => {
+    setWidth(
+      sliderWrapper.current.scrollWidth - sliderWrapper.current.offsetWidth
+    );
+
+    console.log(width);
+  }, []);
   return (
     <div className="hero-container">
       <div className="hero-left-container">
         <div className="hero-left">
           <header>
             <h1>Welcome, Friend</h1>
+
             <h2>Stuck in tutorial hell?</h2>
           </header>
 
@@ -24,11 +42,24 @@ function Hero() {
       </div>
 
       <div className="hero-right-container">
-        <div className="hero-right">
-          <div className="card card1">Card 1</div>
-          <div className="card card2">Card 2</div>
-          <div className="card card3">Card 3</div>
-        </div>
+        <motion.div
+          className="hero-right"
+          ref={sliderWrapper}
+          whileTap={{ cursor: "grabbing" }}
+        >
+          {cards.map((card, index) => {
+            return (
+              <motion.div
+                key={card.id}
+                className={card.className}
+                drag={card.draggable}
+                dragConstraints={{ right: 0, left: -width }}
+              >
+                {card.name}
+              </motion.div>
+            );
+          })}
+        </motion.div>
       </div>
     </div>
   );
