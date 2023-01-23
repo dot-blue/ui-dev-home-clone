fec4c2fad0ca323c67139197d4ec228d2b7d0def
iwritecodesometimes <121631567+dot-blue@users.noreply.github.com>
2023-01-23 19:06:13 +0200

    c

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,46 @@
 import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
+import Malak from "../assets/malak-profile.webp";
+import Clarissa from "../assets/clarissa-profile.webp";
+import Faraz from "../assets/faraz-profile.webp";
+import Star from "../assets/star.svg";
+
 function Hero() {
   const [width, setWidth] = useState(0);
   const sliderWrapper = useRef();
 
   const cards = [
-    { id: 1, className: "card  card-active", name: "card1", draggable: "x" },
-    { id: 2, className: "card  card-left", name: "card2", draggable: "" },
-    { id: 3, className: "card  card-right", name: "card3", draggable: "" },
+    {
+      id: 1,
+      className: "card  card-active",
+      name: "Malak Joseph",
+      draggable: "x",
+      course: "Typescript",
+      testimonial:
+        "“I hated TypeScript when I dealt with it at work because I didn’t know how to use it and only produced red lines! Now, can’t say I’m an expert but an app without TS is full of distractions and more complicated!”",
+      image: Malak,
+    },
+    {
+      id: 2,
+      className: "card  card-left",
+      name: "Clarissa Gayton",
+      draggable: "",
+      course: "Modern Javascript",
+      testimonial:
+        "“What a gem! I’ve just completed a bootcamp and have been studying on my own for quite some time now. I spent a few weeks doing research on the best online courses ... and I’m sad to say I didn’t find Tyler McGinnis earlier.”",
+      image: Clarissa,
+    },
+    {
+      id: 3,
+      className: "card  card-right",
+      name: "Simon Vrachliotis",
+      draggable: "",
+      course: "React Hooks",
+      testimonial:
+        "“I already knew React hooks, but this course really consolidated my understanding of them. I teach React training workshops myself, and after taking this course, my confidence in talking about hooks is much stronger.”",
+      image: Faraz,
+    },
   ];
 
   useEffect(() => {
@@ -55,7 +87,35 @@ function Hero() {
                 drag={card.draggable}
                 dragConstraints={{ right: 0, left: -width }}
               >
-                {card.name}
+                {
+                  <div className="card-container">
+                    <header className="card-header">
+                      <span className="card-elipse">
+                        <img src={card.image} alt="" />
+                      </span>
+
+                      <div className="name-container">
+                        <div className="name-container--inner">
+                          <div>
+                            <h3>{card.name}</h3>
+                            <p>{card.course}</p>
+                          </div>
+                          <div className="star-container">
+                            <img src={Star} alt="" />
+                            <img src={Star} alt="" />
+                            <img src={Star} alt="" />
+                            <img src={Star} alt="" />
+                            <img src={Star} alt="" />
+                          </div>
+                        </div>
+                      </div>
+                    </header>
+                    <div className="card-testimonial">
+                      {" "}
+                      <p>{card.testimonial}</p>
+                    </div>
+                  </div>
+                }
               </motion.div>
             );
           })}
